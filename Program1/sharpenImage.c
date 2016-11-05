bdac93f1cc82cca501042e71c2f0a2de673c1abc
Michael Pfeifer <mikepf97@gmail.com>
2016-11-05 16:09:33 -0600

    Started sharpen

--- Program1/sharpenImage.c
+++ Program1/sharpenImage.c
@@ -4,42 +4,43 @@
 
 /* Apply the sharpen mask to create a new array.  Free the original
    and return the new array */
-static unsigned char ** sharpen_array(unsigned char **a, int rows,int cols)
+unsigned char ** sharpen_array(unsigned char **a, int rows,int cols);
+/*
 {
   int i,j;
   unsigned char **tmp;
 
   tmp = allocate_pixel_array(rows,cols);
 
-  for( i=1; i<rows-1; i++)
-    for( j=1; j<cols-1; j++ )
+  rows--;
+  cols--;
+
+  for( i=1; i<rows; i++)
+    for( j=1; j<cols; j++ )
       tmp[i][j] = saturate( 5 * a[i][j] -
 			    ((int) a[i-1][j]+a[i][j-1]+a[i][j+1]+a[i+1][j]));
 
-  /* fix up the corners */
   tmp[0][0] = saturate( 2*a[0][0] - a[0][1] - a[1][0]);
-  tmp[0][cols-1] = saturate( 2*a[0][cols-1] - a[0][cols-2] - a[1][cols-1]);
-  tmp[rows-1][0] = saturate( 2*a[rows-1][0] - a[rows-1][1] - a[rows-2][0]);
-  tmp[rows-1][cols-1] = saturate( 2*a[rows-1][cols-1] - a[rows-1][cols-2] - a[rows-2][cols-1]);
+  tmp[0][cols] = saturate( 2*a[0][cols] - a[0][cols-1] - a[1][cols]);
+  tmp[rows][0] = saturate( 2*a[rows][0] - a[rows][1] - a[rows-1][0]);
+  tmp[rows][cols] = saturate( 2*a[rows][cols] - a[rows][cols-1] - a[rows-1][cols]);
   
-  /* fix up the left and right borders */
-  for(i=1;i<rows-1;i++)
+  for(i=1;i<rows;i++)
     {    
       tmp[i][0] = saturate( 3*a[i][0] - a[i][1] - a[i-1][0] - a[i+1][0]);
-      tmp[i][cols-1] = saturate( 3*a[i][cols-1] - a[i][cols-2] - a[i-1][cols-1] - a[i+1][cols-1]);
+      tmp[i][cols] = saturate( 3*a[i][cols] - a[i][cols] - a[i-1][cols] - a[i+1][cols]);
     }
 
-  /* fix up the top and bottom borders */
   for(i=1;i<cols-1;i++)
     {    
       tmp[0][i] = saturate( 3*a[0][i] - a[0][i-1] - a[0][i+1] - a[1][i]);
-      tmp[rows-1][i] = saturate( 3*a[rows-1][i] - a[rows-1][i-1] - a[rows-1][i+1] - a[rows-2][i]);
+      tmp[rows][i] = saturate( 3*a[rows][i] - a[rows][i-1] - a[rows][i+1] - a[rows-1][i]);
     }
 
-  free_pixel_array(a,rows);
+  free_pixel_array(a,rows+1);
   return tmp;
 }
-
+*/
 
 void sharpenImage( netImage *image)
 {
